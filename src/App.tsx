4d220d9df14d863d74539d9e3c6c8ce5c87c99e2
Navaneeth CT <navaneethct1199@gmail.com>
2024-04-14 02:09:14 +0530

    Style input and date picker container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import { DateRangePicker } from "./components/date-range-picker";
 
-export function App() {
-  return (
-    <main className="grid h-full place-items-center">
-      <DateRangePicker />
-    </main>
-  );
-}
+const ranges = [
+  {
+    label: "Today",
+    value: [new Date(), new Date()],
+  },
+  {
+    label: "Yesterday",
+    value: [new Date(), new Date()],
+  },
+  {
+    label: "Last 7 Days",
+    value: [new Date(), new Date()],
+  },
+];
+
+export const App = () => (
+  <main className="h-full bg-zinc-950 p-8 text-zinc-300">
+    <DateRangePicker ranges={ranges} />
+  </main>
+);
--- src/components/date-range-picker.tsx
+++ src/components/date-range-picker.tsx
@@ -1,31 +1,124 @@
-import { useState } from "react";
+import { useRef, useState, useEffect, useCallback } from "react";
+import clsx from "clsx";
 
 const placeholder = "yyyy-MM-dd ~ yyyy-MM-dd";
 
-export function DateRangePicker() {
+export type DateRangePickerProps = Readonly<{
+  ranges: Array<{
+    label: string;
+    value: Date[];
+  }>;
+}>;
+
+export const DateRangePicker = ({ ranges }: DateRangePickerProps) => {
+  const element = useRef<HTMLDivElement>(null);
+
   const [value, setValue] = useState("");
+  const [draft, setDraft] = useState(placeholder);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  function handleFocus() {
+  const open = () => setIsOpen(true);
+  const close = () => setIsOpen(false);
+
+  const focus = () => {
+    setIsFocused(true);
+    open();
+  };
+  const blur = useCallback(() => {
+    setIsFocused(false);
+    close();
+  }, []);
+
+  useEffect(() => {
+    const handleBlur = (event: MouseEvent) => {
+      if (!element.current?.contains(event.target as Node)) blur();
+    };
+
+    document.addEventListener("mousedown", handleBlur);
+    return () => document.removeEventListener("mousedown", handleBlur);
+  }, [blur]);
+
+  const handleInputFocus = () => {
+    focus();
     if (!value) setValue(placeholder);
-  }
+  };
 
-  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    close();
     setValue(event.target.value);
-  }
+  };
 
-  function handleBlur() {
+  const handleInputBlur = () => {
     if (value === placeholder) setValue("");
-  }
+  };
+
+  const handleRangeSelect = (range: Date[]) => {
+    blur();
+  };
+
+  const handleSubmit = () => {
+    blur();
+  };
 
   return (
-    <div>
+    <div
+      ref={element}
+      className={clsx(
+        "relative flex h-9 w-64 items-center rounded-md border bg-zinc-900 transition-all duration-200 hover:border-sky-400",
+        { "border-sky-400": isFocused, "border-zinc-700": !isFocused },
+      )}
+    >
       <input
         placeholder={placeholder}
         value={value}
-        onFocus={handleFocus}
-        onChange={handleChange}
-        onBlur={handleBlur}
+        onFocus={handleInputFocus}
+        onChange={handleInputChange}
+        onBlur={handleInputBlur}
+        spellCheck={false}
+        className="flex-grow bg-transparent py-2 pl-3 text-sm text-zinc-200 outline-none placeholder:text-zinc-400"
       />
+      <button
+        onFocus={focus}
+        className={clsx("h-full w-8 flex-shrink-0 outline-none", {
+          "cursor-pointer": value && value !== placeholder,
+          "cursor-text": !value || value === placeholder,
+        })}
+      ></button>
+
+      <div
+        className={clsx(
+          "absolute -left-px top-full mt-px min-w-[36rem] rounded-md bg-zinc-800 shadow",
+          { hidden: !isFocused || !isOpen },
+        )}
+      >
+        <header className="px-3 py-2 text-sm">{draft}</header>
+
+        <section className="grid grid-cols-2 border-b border-t border-zinc-700">
+          <div className="border-r border-zinc-700 py-3"></div>
+          <div className="py-3"></div>
+        </section>
+
+        <footer className="flex items-start justify-between p-3">
+          <div className="flex max-w-[400px] flex-grow flex-wrap gap-1">
+            {ranges.map((range) => (
+              <button
+                key={range.label}
+                onClick={() => handleRangeSelect(range.value)}
+                className="px-2.5 py-[5px] text-sm text-sky-400 hover:underline"
+              >
+                {range.label}
+              </button>
+            ))}
+          </div>
+          <button
+            onClick={handleSubmit}
+            className="rounded-md bg-sky-500 px-2.5 py-[5px] text-sm text-white transition-all duration-200 hover:bg-sky-400"
+          >
+            OK
+          </button>
+        </footer>
+      </div>
     </div>
   );
-}
+};
